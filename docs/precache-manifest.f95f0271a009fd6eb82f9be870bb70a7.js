2bde903a0e0af12d264e1b85baf682f7edd9fcad
Leabiv <leabiv@gmail.com>
2020-07-27 09:11:36 -0500

    docs agregado

---
+++ docs/precache-manifest.f95f0271a009fd6eb82f9be870bb70a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8f2023e90c56315f11fe71a154f601e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3cbd3387d542aa0144ee",
+    "url": "/static/css/main.16661450.chunk.css"
+  },
+  {
+    "revision": "027ae5944804431f3db5",
+    "url": "/static/js/2.9f575afa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9f575afa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cbd3387d542aa0144ee",
+    "url": "/static/js/main.a862c42d.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
